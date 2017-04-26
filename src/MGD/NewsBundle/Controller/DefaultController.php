a45af168e16fc60a9cf9d74664ae8fa8623e207e
acastelain <castelain.alexandre@outlook.com>
2017-04-26 11:34:22 +0200

    Created the base for the news (they're displayed in the index)

--- src/MGD/NewsBundle/Controller/DefaultController.php
+++ src/MGD/NewsBundle/Controller/DefaultController.php
@@ -8,6 +8,30 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MGDNewsBundle:Default:index.html.twig');
+        $news = array(
+            array(
+                "title" => "Titre 1",
+                "body"  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla grav",
+                "creationDate"  => new \DateTime(),
+                "creator"       => array("name"=>"Michel", "firstname"=>"Michel"),
+                "image"         => "http://placehold.it/200x150"
+            ),
+            array(
+                "title" => "Titre 2",
+                "body"  => "<b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla grav",
+                "creationDate"  => new \DateTime(),
+                "creator"       => array("name"=>"Nom", "firstname"=>"Prénom"),
+                "image"         => "http://placehold.it/500x1500"
+            ),
+            array(
+                "title" => "Titre 3",
+                "body"  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla gravLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor dui eu interdum scelerisque. Nunc congue nulla et arcu dignissim molestie. Donec iaculis eu erat sit amet scelerisque. Donec tincidunt, quam ac molestie elementum, dui dui scelerisque ante, a imperdiet magna metus at odio. Nulla grav",
+                "creationDate"  => new \DateTime(),
+                "creator"       => array("name"=>"Jean", "firstname"=>"Paul"),
+                "image"         => null
+            ),
+        );
+        return $this->render('MGDNewsBundle:Default:index.html.twig',
+            array("listNews"  => $news));
     }
 }
